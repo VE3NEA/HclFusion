db06a93c097d5e70999c96f535b81802e11a8921
VE3NEA <alshovk@dxatlas.com>
2019-08-21 19:31:08 -0400

    Initial upload

--- HclFusion/Form1.Designer.cs
+++ HclFusion/Form1.Designer.cs
@@ -28,13 +28,229 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.trackBar2 = new System.Windows.Forms.TrackBar();
+      this.label4 = new System.Windows.Forms.Label();
+      this.trackBar1 = new System.Windows.Forms.TrackBar();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.OpenVisibleFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.OpenThermalFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.SaveCompositeFileDialog = new System.Windows.Forms.SaveFileDialog();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.Controls.Add(this.button4);
+      this.panel1.Controls.Add(this.button3);
+      this.panel1.Controls.Add(this.trackBar2);
+      this.panel1.Controls.Add(this.label4);
+      this.panel1.Controls.Add(this.trackBar1);
+      this.panel1.Controls.Add(this.label3);
+      this.panel1.Controls.Add(this.textBox2);
+      this.panel1.Controls.Add(this.button2);
+      this.panel1.Controls.Add(this.label2);
+      this.panel1.Controls.Add(this.textBox1);
+      this.panel1.Controls.Add(this.button1);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panel1.Location = new System.Drawing.Point(0, 0);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(259, 573);
+      this.panel1.TabIndex = 0;
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(90, 333);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(75, 23);
+      this.button4.TabIndex = 8;
+      this.button4.Text = "Save";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(90, 291);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 23);
+      this.button3.TabIndex = 7;
+      this.button3.Text = "Fuse";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // trackBar2
+      // 
+      this.trackBar2.Location = new System.Drawing.Point(12, 230);
+      this.trackBar2.Maximum = 100;
+      this.trackBar2.Name = "trackBar2";
+      this.trackBar2.Size = new System.Drawing.Size(236, 45);
+      this.trackBar2.TabIndex = 6;
+      this.trackBar2.TickFrequency = 10;
+      this.trackBar2.Value = 50;
+      this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(12, 214);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(66, 13);
+      this.label4.TabIndex = 9;
+      this.label4.Text = "Chroma 50%";
+      // 
+      // trackBar1
+      // 
+      this.trackBar1.Location = new System.Drawing.Point(12, 170);
+      this.trackBar1.Maximum = 300;
+      this.trackBar1.Name = "trackBar1";
+      this.trackBar1.Size = new System.Drawing.Size(236, 45);
+      this.trackBar1.TabIndex = 5;
+      this.trackBar1.TickFrequency = 20;
+      this.trackBar1.Value = 100;
+      this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 154);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(76, 13);
+      this.label3.TabIndex = 7;
+      this.label3.Text = "Hue Gain 1.00";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(12, 85);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ReadOnly = true;
+      this.textBox2.Size = new System.Drawing.Size(205, 20);
+      this.textBox2.TabIndex = 3;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(223, 84);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(25, 23);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "...";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 69);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(153, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Thermal Image, gray scale only";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(12, 28);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ReadOnly = true;
+      this.textBox1.Size = new System.Drawing.Size(205, 20);
+      this.textBox1.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(223, 27);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(25, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "...";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 12);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(161, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Visible Image, gray scale or color";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+      this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.pictureBox1.Location = new System.Drawing.Point(259, 0);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(764, 573);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+      this.pictureBox1.TabIndex = 1;
+      this.pictureBox1.TabStop = false;
+      // 
+      // OpenVisibleFileDialog
+      // 
+      this.OpenVisibleFileDialog.DefaultExt = "bmp";
+      this.OpenVisibleFileDialog.FileName = "openFileDialog1";
+      this.OpenVisibleFileDialog.Title = "Select Visible Image";
+      // 
+      // OpenThermalFileDialog
+      // 
+      this.OpenThermalFileDialog.DefaultExt = "bmp";
+      this.OpenThermalFileDialog.FileName = "openFileDialog2";
+      this.OpenThermalFileDialog.Title = "Select Thermal Image";
+      // 
+      // SaveCompositeFileDialog
+      // 
+      this.SaveCompositeFileDialog.DefaultExt = "bmp";
+      this.SaveCompositeFileDialog.Title = "Save Composite File";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(1023, 573);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.panel1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "HCL Fusion";
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    public System.Windows.Forms.OpenFileDialog OpenVisibleFileDialog;
+    public System.Windows.Forms.OpenFileDialog OpenThermalFileDialog;
+    public System.Windows.Forms.TrackBar trackBar2;
+    public System.Windows.Forms.TrackBar trackBar1;
+    public System.Windows.Forms.TextBox textBox2;
+    public System.Windows.Forms.TextBox textBox1;
+    public System.Windows.Forms.Label label4;
+    public System.Windows.Forms.Label label3;
+    private System.Windows.Forms.SaveFileDialog SaveCompositeFileDialog;
   }
 }
 
--- HclFusion/Form1.cs
+++ HclFusion/Form1.cs
@@ -1,20 +1,178 @@
-﻿using System;
+﻿using Colourful;
+using Colourful.Conversion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace HclFusion
 {
   public partial class Form1 : Form
   {
+    private double[] visibles, thermals;
+    private Bitmap bitmap;
+    double hueGain = 1;
+    double chroma = 50;
+
+    //----------------------------------------------------------------------------------------------
+    //                                       system
+    //----------------------------------------------------------------------------------------------
     public Form1()
     {
       InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (OpenVisibleFileDialog.ShowDialog() == DialogResult.OK)
+        textBox1.Text = Path.GetFileName(OpenVisibleFileDialog.FileName);
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if (OpenThermalFileDialog.ShowDialog() == DialogResult.OK)
+        textBox2.Text = Path.GetFileName(OpenThermalFileDialog.FileName);
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      FuseImages();
+      pictureBox1.Image = bitmap;
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      if (SaveCompositeFileDialog.ShowDialog() == DialogResult.OK)
+        bitmap.Save(SaveCompositeFileDialog.FileName, ImageFormat.Bmp);
+    }
+
+    private void trackBar1_ValueChanged(object sender, EventArgs e)
+    {
+      hueGain = trackBar1.Value / 100d; //0..300 -> 0.0..3.0
+      chroma = trackBar2.Value;         //0..100
+
+      label3.Text = $"Hue Gain {hueGain:F2}";
+      label4.Text = $"Chroma {(chroma):F0}%";
+    }
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                     HCL Fusion
+    //----------------------------------------------------------------------------------------------
+    private void FuseImages()
+    {
+      LoadImage(OpenThermalFileDialog.FileName, false);
+      thermals = GetImageLuminance();
+
+      LoadImage(OpenVisibleFileDialog.FileName, true);
+      visibles = GetImageLuminance();
+
+      FuseLuminances();
+    }
+
+    //this is the core of the algorithm. the output image is computed in the HCL color space 
+    //using the gray scale visible image for Luminance and thermal image for Hue
+    private unsafe void FuseLuminances()
+    {
+      var converter = new ColourfulConverter { WhitePoint = Illuminants.D65 };
+
+      var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+        ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+      int* dst = (int*)data.Scan0;
+
+      for (int i = 0; i < visibles.Length; i++)
+      {
+        double luminance = visibles[i] * 100; //0..100
+        double hue =Math.Min(1, thermals[i] * hueGain);
+        hue = 300 - (int)(hue * 300); //0..300
+
+        //compose HCL color
+        var hclColor = new LChabColor(luminance, chroma, hue);
+
+        //convert to RGB and put in bitmap
+        Color color = converter.ToRGB(hclColor);
+        *dst++ = (color.R << 16) | (color.G << 8) | color.B;
+      }
+      bitmap.UnlockBits(data);
+    }
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                  image loading
+    //----------------------------------------------------------------------------------------------
+
+    //load image and extract luminance
+    private unsafe double[] GetImageLuminance()
+    {
+      //prepare to convert between RGB and HCL
+      var converter = new ColourfulConverter { WhitePoint = Illuminants.D65 };
+      double[] result = new double[bitmap.Width * bitmap.Height];
+
+      //prepare to read bitmap
+      var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+        ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+      int* src = (int*)data.Scan0;
+
+      for (int pix = 0; pix < result.Length; pix++)
+      {
+        //read RGB color
+        var intValue = *src++;
+        var color = Color.FromArgb((byte)(intValue >> 16), (byte)(intValue >> 8), (byte)intValue);
+
+        //convert to HCL
+        var hclColor = converter.ToLChab((RGBColor)color);
+
+        //take luminance (0..100)
+        result[pix] = hclColor.L;
+      }
+      bitmap.UnlockBits(data);
+
+      //normalize values
+      var min = result.Min();
+      var max = result.Max();
+      var scale = 1.0 / (max - min);
+      for (int i = 0; i < result.Length; i++) result[i] = (result[i] - min) * scale;
+
+      return result;
+    }
+
+    //load regular image
+    private void LoadImage(string path, bool stretch)
+    {
+      var image = (Path.GetExtension(path).ToLower() == ".tiff") ? LoadThermalTiff(path) : Bitmap.FromFile(path);
+
+      //force 32-bit RGB format and optionally adjust image size
+      if (!stretch) bitmap = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppRgb);
+      using (var g = Graphics.FromImage(bitmap)) { g.DrawImage(image, 0, 0, bitmap.Width, bitmap.Height); }
+    }
+
+    //load thermal data from a tiff file created with Seek Thermal App.
+    //such files contain three frames, the last frame is thermal data.
+    private Bitmap LoadThermalTiff(string path)
+    {
+      //read tiff frame
+      var inStream = new FileStream(path, FileMode.Open);
+      var decoder = new TiffBitmapDecoder(inStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+      var frame = BitmapFrame.Create(decoder.Frames[decoder.Frames.Count - 1]);
+
+      //write bitmap
+      BitmapEncoder encoder = new BmpBitmapEncoder();
+      encoder.Frames.Add(frame);
+      var outStream = new MemoryStream();
+      encoder.Save(outStream);
+      return new Bitmap(outStream);
     }
   }
 }
